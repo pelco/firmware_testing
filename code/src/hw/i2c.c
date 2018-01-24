0a42b09c12d56add85e69e823c3e86d35f73ee91
pelco <pelco89@gmail.com>
2018-01-24 19:46:40 +0000

    added i2c dummy hardware funtions

---
+++ code/src/hw/i2c.c
@@ -0,0 +1,22 @@
+/**
+ * This is file implements i2c hardware funcions.
+ */
+
+#include <stdio.h>
+#include "common.h"
+
+/**
+ * I2C Read
+ */
+uint8_t i2c_read(uint8_t address)
+{
+    return 0;
+}
+
+/**
+ * I2C Write
+ */
+uint8_t i2c_write(uint8_t address, uint8_t value)
+{
+    return 0;
+}
