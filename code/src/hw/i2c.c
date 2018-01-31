7048a1adfed4f740415b0c27889f11f5496f2df5
pelco <pelco89@gmail.com>
2018-01-31 21:44:28 +0000

    documented i2c hw functions

--- code/src/hw/i2c.c
+++ code/src/hw/i2c.c
@@ -1,22 +1,29 @@
 /**
- * This is file implements i2c hardware funcions.
+ * This file implements i2c hardware functions. These funcions are hardware
+ * specific and are supplied by the vendor SoC vendor.
+ * These functions are hard
  */
 
 #include <stdio.h>
 #include "common.h"
 
 /**
- * I2C Read
+ * 
+ * This implements i2c read hw function. The function is never called
+ * in the unit test because it's mocked by the CppUTest framework.
  */
 uint8_t i2c_read(uint8_t address)
 {
+    printf("I2C read at address 0x%x\n", address);
     return 0;
 }
 
 /**
- * I2C Write
+ * I2C Write implements a hardware function that is never called in the
+ * unit test. They are mocked by the CppUTest Framework.
  */
-uint8_t i2c_write(uint8_t address, uint8_t value)
+uint8_t i2c_write(uint8_t value, uint8_t address)
 {
+    printf("I2C write value 0x%x at address 0x%x\n", value, address);
     return 0;
 }
