ebafb98d117262ded790fb513c233d229203e59f
pelco <pelco89@gmail.com>
2018-01-31 22:01:08 +0000

    update documented i2c hw functions

--- code/src/hw/i2c.c
+++ code/src/hw/i2c.c
@@ -2,15 +2,15 @@
  * This file implements the i2c hardware functions and are provided by the
  * SoC vendor. This type of functions (architecture specific) make embedded
  * systems debugging difficult and painful.
+ * These functions are mocked in tests/mocks/i2c_mock.cpp by the CppUTest
+ * framework and therefore never called.
  */
 
 #include <stdio.h>
 #include "common.h"
 
 /**
- * 
- * This implements i2c read hw function. The function is never called
- * in the unit test because it's mocked by the CppUTest framework.
+ * This implements i2c read hw function.
  */
 uint8_t i2c_read(uint8_t address)
 {
@@ -19,8 +19,7 @@ uint8_t i2c_read(uint8_t address)
 }
 
 /**
- * I2C Write implements a hardware function that is never called in the
- * unit test. They are mocked by the CppUTest Framework.
+ * This implements i2c write hw function.
  */
 uint8_t i2c_write(uint8_t value, uint8_t address)
 {
