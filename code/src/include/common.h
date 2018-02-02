51c4ce055569dcbefb7e21dd77688db3fdaab701
pelco <pelco89@gmail.com>
2018-02-02 21:16:48 +0000

    added i2c funcions to common header

--- code/src/include/common.h
+++ code/src/include/common.h
@@ -6,7 +6,11 @@
 
 uint8_t calculator(char op, uint8_t val1, uint8_t val2);
 
-/* Hardware functions from hw/i2c.c */
+/* I2C Hardware functions and slave registers */
+const uint8_t I2C_SLAVE_ADDRESS = 0x30;
+const uint8_t I2C_REG1 = 0x0A;
+const uint8_t I2C_REG2 = 0x0B;
+
 uint8_t i2c_read(uint8_t address);
 uint8_t i2c_write(uint8_t address, uint8_t value);
 
