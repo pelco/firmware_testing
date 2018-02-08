abdca7bbdeb8e5a98717174b791657b1e3b3b516
pelco <pelco89@gmail.com>
2018-02-08 19:12:09 +0000

    changed firmware and test code

--- code/src/main.c
+++ code/src/main.c
@@ -14,7 +14,7 @@ void init_device(void)
 }
 
 /** 
- * TEST_MAIN macro allows us to test firmware code in tests/t_main.cpp 
+ * TEST_MAIN macro allows to test firmware code in tests/t_main.cpp
  */
 #ifdef TEST_MAIN
 uint32_t test_main(void)
@@ -29,11 +29,12 @@ uint32_t main(void)
     init_device();
 
     /**
-     * 
+     * Use case: Firmware tries to read I2C_REG2, if successful it
+     * will read I2C_REG3, otherwise tries to configure device again.
      */
     reg = i2c_read(I2C_SLAVE_ADDRESS, I2C_REG2);
     if (reg > 0x10) {
-        i2c_read(I2C_SLAVE_ADDRESS, I2C_REG2);
+        i2c_read(I2C_SLAVE_ADDRESS, I2C_REG3);
     } else {
         init_device();
     }
